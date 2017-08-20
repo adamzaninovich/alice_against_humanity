da8d1ec951a3c50bb008e02fca543df23f31a0c6
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-08-19 21:48:38 -0700

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AliceAgainstHumanity.Mixfile do
 
   def project do
     [app: :alice_against_humanity,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
